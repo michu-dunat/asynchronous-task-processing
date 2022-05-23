e670208297b0de2c8c44a1a1e4387042fdbdf5b6
Michał Dunat <kontaktmichaldunat@gmail.com>
2022-05-23 14:09:09 +0200

    add initial project

---
+++ src/main/java/com/example/asynchronoustaskprocessing/AsynchronousTaskProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.asynchronoustaskprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousTaskProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynchronousTaskProcessingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/asynchronoustaskprocessing/AsynchronousTaskProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asynchronoustaskprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchronousTaskProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
